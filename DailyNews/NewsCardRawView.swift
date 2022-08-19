380d63e786313e5ba5f9e7784d7c490ea694c2ac
sisithajayawardhane <sisithajayawardhane@gmail.com>
2022-08-19 13:12:51 +0530

    added news card raw view

---
+++ DailyNews/NewsCardRawView.swift
@@ -0,0 +1,20 @@
+//
+//  NewsCardRawView.swift
+//  DailyNews
+//
+//  Created by mac on 2022-08-19.
+//
+
+import SwiftUI
+
+struct NewsCardRawView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewsCardRawView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsCardRawView()
+    }
+}
